161263cb38c16ca2a5b54938c8165a86c306c550
Marcel <htrpmarck@gmail.com>
2025-03-05 04:04:09 +0000

    Added Create Workout button with demo functionality

--- app/src/main/java/com/example/gymtracker/MainActivity.kt
+++ app/src/main/java/com/example/gymtracker/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gymtracker.ui.theme.GymTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             GymTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "User",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CreateWorkoutButton()
+                    }
                 }
             }
         }
@@ -31,17 +41,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateWorkoutButton() {
+    Button(
+        onClick = { println("Create Workout button clicked!") },
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text("Create Workout")
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CreateWorkoutButtonPreview() {
     GymTrackerTheme {
-        Greeting("Android")
+        CreateWorkoutButton()
     }
 }
